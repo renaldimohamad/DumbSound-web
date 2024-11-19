797ef837b5ec2f99e6ff6d8cfcb5c7b1662ed2ee
Sundus <sundusardian@gmail.com>
2024-11-19 23:40:44 +0700

    fix(adds global state to all components)

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,12 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
-import "./index.css";
+import { StoreProvider } from "./stores/index.tsx";
 
 createRoot(document.getElementById("root")!).render(
    <StrictMode>
-      <App />
+      <StoreProvider>
+      <App/>
+      </StoreProvider>
    </StrictMode>
 );
